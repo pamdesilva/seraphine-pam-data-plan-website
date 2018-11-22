9fdd9dbfc9771601a686f3b003538f97492fc995
Pamela De Silva Michael <pam.desilva@gmail.com>
2018-11-22 09:00:06 +0000

    added total summary to chatterbox and family plans

--- js/app.js
+++ js/app.js
@@ -4,31 +4,35 @@ var loneWolfSelectedPrice;
 var loneWolfTotal;
 var loneWolfMonthly = true;
 
-$('.lone-wolf-card').click(function(e) {
+$(".lone-wolf-card").click(function(e) {
   e.preventDefault();
-  loneWolfSelectedPrice = $(this).data('value');
+  loneWolfSelectedPrice = $(this).data("value");
 
   displayLoneWolfTotal(loneWolfSelectedPrice, loneWolfMonthly);
-  $('#lone-wolf-frequency-section').removeClass('hide').addClass('show');
-  $('#lone-wolf-total-section').removeClass('hide').addClass('show');
+  $("#lone-wolf-frequency-section")
+    .removeClass("hide")
+    .addClass("show");
+  $("#lone-wolf-total-section")
+    .removeClass("hide")
+    .addClass("show");
 });
 
-$('.lone-wolf-frequency').click(function(e) {
+$(".lone-wolf-frequency").click(function(e) {
   e.preventDefault();
-  var btnType = $(this).text().toLowerCase();
+  var btnType = $(this)
+    .text()
+    .toLowerCase();
 
-  if (btnType === 'annually') {
+  if (btnType === "annually") {
     loneWolfMonthly = false;
   } else {
     loneWolfMonthly = true;
   }
 
   displayLoneWolfTotal(loneWolfSelectedPrice, loneWolfMonthly);
-
 });
 
 function displayLoneWolfTotal(price, type) {
-
   if (loneWolfMonthly == true) {
     loneWolfTotal = price;
     $("#lone-wolf-plan-type").html("Monthly");
@@ -43,45 +47,73 @@ function displayLoneWolfTotal(price, type) {
 ////////////////////////////// Chatterbox page ////////////////////////////////////
 
 var chatterboxSelectedGB;
+var chatterboxGbNum;
 var chatterboxSelectedMin;
+var chatterboxMinNum;
 var chatterboxTotal;
 var chatterboxMonthly = true;
 
 $(".chatterbox-gb").click(function(e) {
   e.preventDefault();
-  $('#chatterbox-mins-section').removeClass('hide').addClass('show');
+  $("#chatterbox-mins-section")
+    .removeClass("hide")
+    .addClass("show");
   chatterboxSelectedGB = $(this).data("price");
-  $('.chatterbox-min').removeAttr('disabled');
+  chatterboxGbNum = $(this)
+    .find("p")
+    .text();
+  console.log(chatterboxGbNum);
+  $(".chatterbox-min").removeAttr("disabled");
 
   if (chatterboxSelectedMin) {
-    displayChatterboxTotal(chatterboxSelectedGB, chatterboxSelectedMin, chatterboxMonthly);
+    displayChatterboxTotal(
+      chatterboxSelectedGB,
+      chatterboxSelectedMin,
+      chatterboxMonthly
+    );
   }
 });
 
 $(".chatterbox-min").click(function(e) {
   e.preventDefault();
-  $('#chatterbox-frequency-section').removeClass('hide').addClass('show');
-  $('#chatterbox-total-section').removeClass('hide').addClass('show');
+  $("#chatterbox-frequency-section")
+    .removeClass("hide")
+    .addClass("show");
+  $("#chatterbox-total-section")
+    .removeClass("hide")
+    .addClass("show");
   chatterboxSelectedMin = $(this).data("price");
+  chatterboxMinNum = $(this)
+    .find("p")
+    .text();
 
-  displayChatterboxTotal(chatterboxSelectedGB, chatterboxSelectedMin, chatterboxMonthly);
+  displayChatterboxTotal(
+    chatterboxSelectedGB,
+    chatterboxSelectedMin,
+    chatterboxMonthly
+  );
 
-  $('#chatterbox-total-section').show();
-
+  $("#chatterbox-total-section").show();
 });
 
-$('.chatterbox-frequency').click(function(e) {
+$(".chatterbox-frequency").click(function(e) {
   e.preventDefault();
 
-  var btnType = $(this).text().toLowerCase();
+  var btnType = $(this)
+    .text()
+    .toLowerCase();
 
-  if (btnType === 'annually') {
+  if (btnType === "annually") {
     chatterboxMonthly = false;
   } else {
     chatterboxMonthly = true;
   }
 
-  displayChatterboxTotal(chatterboxSelectedGB, chatterboxSelectedMin, chatterboxMonthly);
+  displayChatterboxTotal(
+    chatterboxSelectedGB,
+    chatterboxSelectedMin,
+    chatterboxMonthly
+  );
 });
 
 function displayChatterboxTotal(gb, min, type) {
@@ -96,6 +128,12 @@ function displayChatterboxTotal(gb, min, type) {
   }
 
   $("#chatterbox-plan-total").html("Total: £" + chatterboxTotal);
+  $("#chatterbox-summary-min").html(
+    chatterboxMinNum + "  –  £" + chatterboxSelectedMin
+  );
+  $("#chatterbox-summary-gb").html(
+    chatterboxGbNum + "  –  £" + chatterboxSelectedGB
+  );
 }
 
 ////////////////////////////// Family page ////////////////////////////////////
@@ -103,9 +141,9 @@ function displayChatterboxTotal(gb, min, type) {
 var familySimNumber;
 var pricesToDisplay = [];
 var familySelectedPrice;
+var familySelectedGb;
 var familyTotal;
 var familyMonthly = true;
-var lastClickedPrice;
 var priceTiers = [
   [14, 20, 30, 40],
   [21.2, 32, 50, 68],
@@ -114,59 +152,88 @@ var priceTiers = [
   [42.8, 68, 110, 152]
 ];
 
-$('.family-sim-number').click(function(e) {
+$(".family-sim-number").click(function(e) {
   e.preventDefault();
-  familySimNumber = $(this).data('price');
+  familySimNumber = $(this).data("price");
 
-  $('#family-gb-section').removeClass('hide').addClass('show');
+  $("#family-gb-section")
+    .removeClass("hide")
+    .addClass("show");
 
   pricesToDisplay = priceTiers.slice(0)[familySimNumber - 1];
 
   pricesToDisplay.forEach(function(value, index) {
-    var price = $('.family-gb-price')[index];
-    $(price).html('£' + value.toFixed(2));
-
-    var gbPrice = $(".family-gb")[index];
-    $(gbPrice).attr("data-price", value)
+    var price = $(".family-gb-price")[index];
+    $(price).html("£" + value.toFixed(2));
   });
 
-  if(familySelectedPrice) {
-    console.log($(this).data('price'));
-    displayFamilyTotal(familySelectedPrice, familyMonthly);
+  if (familySelectedPrice) {
+    familySelectedPrice = 0;
+    familySelectedGb;
+    $("#family-plan-gb").hide();
+    displayFamilyTotal(familySelectedPrice, familyMonthly, familySelectedGb);
+    $("#buy-btn").hide();
+    $("#family-plan-type").hide();
   }
-
 });
 
-$('.family-gb').click(function(e) {
+$(".family-gb").click(function(e) {
   e.preventDefault();
-  $('#family-frequency-section').removeClass('hide').addClass('show');
-  familySelectedPrice = $(this).data("price");
-  displayFamilyTotal(familySelectedPrice, familyMonthly);
-  $('#family-total-section').removeClass('hide').addClass('show');
+  $("#family-frequency-section")
+    .removeClass("hide")
+    .addClass("show");
+
+  familySelectedPrice =
+    $(this)
+      .find("h5")
+      .text()
+      .slice(1, 5) * 1;
+
+  familySelectedGb = $(this)
+    .find("p")
+    .text();
+
+  console.log(familySelectedGb);
+
+  console.log(familySelectedPrice);
+
+  displayFamilyTotal(familySelectedPrice, familyMonthly, familySelectedGb);
+  $("#buy-btn").show();
+  $("#family-plan-gb").show();
+  $("#family-plan-type").show();
+  $("#family-total-section")
+    .removeClass("hide")
+    .addClass("show");
 });
 
-$('.family-frequency').click(function(e) {
+$(".family-frequency").click(function(e) {
   e.preventDefault();
-  var btnType = $(this).text().toLowerCase();
+  var btnType = $(this)
+    .text()
+    .toLowerCase();
 
-  if (btnType === 'monthly'){
+  if (btnType === "monthly") {
     familyMonthly = true;
   } else {
     familyMonthly = false;
   }
 
-  displayFamilyTotal(familySelectedPrice, familyMonthly);
+  displayFamilyTotal(familySelectedPrice, familyMonthly, familySelectedGb);
 });
 
-
-function displayFamilyTotal(price, type) {
-
+function displayFamilyTotal(price, type, gb) {
   if (familyMonthly == true) {
-    familyTotal = price;
-    $("#family-plan-type").html("Monthly");
+    familyTotal = price.toFixed(2);
+    $("#family-plan-type").html("Monthly plan");
   } else {
-    familyTotal = price * 12;
-    $("#family-plan-type").html("Annual");
+    familyTotal = (price * 12).toFixed(2);
+    $("#family-plan-type").html("Annual plan");
+  }
+
+  if (familySimNumber == 1) {
+    $("#family-plan-gb").html("1 sim card – " + gb);
+  } else {
+    $("#family-plan-gb").html(familySimNumber + " sim cards – " + gb + " each");
   }
 
   $("#family-plan-total").html("Total: £" + familyTotal);
